c10980cbffdc4c626d0edd5c55e7d834c0611047
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-04 00:28:26 -0400

    Create 03.ggsurvplot().r

---
+++ Chapter 3/03.ggsurvplot().r
@@ -0,0 +1,10 @@
+### ggsurvplot() versus ggsurvplot_df()
+
+# What is the difference between the ggsurvplot_df() function and the ggsurvplot() function?
+
+# Take a look at the help files of the two functions to determine your answer.
+
+?ggsurvplot_df
+?ggsurvplot
+
+# ggsurvplot_df() allows for argument fit to only be a data.frame, while ggsurvplot() allows different types of objects.
